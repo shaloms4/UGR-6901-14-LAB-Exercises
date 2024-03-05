6c6cf48dc30cd6863fdf57cc1d576169f75a35a1
shaloms4 <shalomhabtamum@gmail.com>
2024-03-05 16:33:13 +0300

    variables and types

---
+++ LAB-1-Variables-and-types(1).dart
@@ -0,0 +1,7 @@
+void main() {
+  int a = 3;
+  int b = 8;
+  int c = 12;
+  int sum = a + b + c;
+  print('The sum of $a, $b, and $c is: $sum');
+}
---
+++ LAB-1-Variables-and-types(2).dart
@@ -0,0 +1,4 @@
+void main() {
+  String str = "Hello, Dart!";
+  print('The length of the string is: ${str.length}');
+}
