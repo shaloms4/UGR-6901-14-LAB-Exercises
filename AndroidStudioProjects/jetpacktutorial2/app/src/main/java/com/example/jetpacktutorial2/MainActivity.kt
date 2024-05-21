fd9c54d0394ff0f9388b2b87620d201dabc41502
shaloms4 <shalomhabtamum@gmail.com>
2024-05-21 23:33:28 +0300

    jetpack

---
+++ AndroidStudioProjects/jetpacktutorial2/app/src/main/java/com/example/jetpacktutorial2/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.jetpacktutorial2
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageScreen()
+        }
+    }
+}
---
+++ AndroidStudioProjects/jetpacktutorial2/app/src/main/java/com/example/jetpacktutorial2/Message.kt
@@ -0,0 +1,3 @@
+package com.example.jetpacktutorial2
+
+data class Message(val author: String, val body: String)
